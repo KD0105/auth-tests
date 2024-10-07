b6c41a1dd29ecb4fd182dd49231e6507868819d6
Danil <danilbeketov@mail.ru>
2024-10-07 12:47:41 +0300

    Create test_successful_login_redirect.py
    Успешный редирект после входа

---
+++ test_successful_login_redirect.py
@@ -0,0 +1,10 @@
+def test_successful_redirect():
+    driver = webdriver.Chrome()
+    driver.get("https://b2c.passport.rt.ru/auth/realms/b2c/protocol/openid-connect/auth?client_id=account_b2c&redirect_uri=https://b2c.passport.rt.ru/account_b2c/login&response_type=code&scope=openid")
+
+    driver.find_element(By.NAME, "login").send_keys("danilbeketov@mail.ru")
+    driver.find_element(By.NAME, "password").send_keys("78965412Kd")
+    driver.find_element(By.NAME, "submit").click()
+
+    assert driver.current_url == "https://b2c.passport.rt.ru/account_b2c/login"
+    driver.quit()
