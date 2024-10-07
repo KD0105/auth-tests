2999f03f6528acd18d474229ae8ca2368fc8a8fe
Danil <danilbeketov@mail.ru>
2024-10-07 12:44:22 +0300

    test_login_success.py

--- test_login_success.py
+++ test_login_success.py
@@ -5,8 +5,8 @@ def test_successful_authentication():
     driver = webdriver.Chrome()
     driver.get("https://b2c.passport.rt.ru/auth/realms/b2c/protocol/openid-connect/auth?client_id=account_b2c&redirect_uri=https://b2c.passport.rt.ru/account_b2c/login&response_type=code&scope=openid")
 
-    driver.find_element(By.NAME, "login").send_keys("valid_username")
-    driver.find_element(By.NAME, "password").send_keys("valid_password")
+    driver.find_element(By.NAME, "login").send_keys("danilbeketov@mail.ru")
+    driver.find_element(By.NAME, "password").send_keys("78965412Kd")
     driver.find_element(By.NAME, "submit").click()
 
     assert driver.current_url == "https://b2c.passport.rt.ru/account_b2c/login"
