3979bf271ba3f98c86e8682d43d0577556106c37
Danil <danilbeketov@mail.ru>
2024-10-07 12:42:54 +0300

    test_unsuccessful_login.py

---
+++ test_unsuccessful_login.py
@@ -0,0 +1,11 @@
+def test_invalid_credentials():
+    driver = webdriver.Chrome()
+    driver.get("https://b2c.passport.rt.ru/auth/realms/b2c/protocol/openid-connect/auth?client_id=account_b2c&redirect_uri=https://b2c.passport.rt.ru/account_b2c/login&response_type=code&scope=openid")
+
+    driver.find_element(By.NAME, "login").send_keys("invalid_username")
+    driver.find_element(By.NAME, "password").send_keys("invalid_password")
+    driver.find_element(By.NAME, "submit").click()
+
+    error_message = driver.find_element(By.ID, "error_message").text
+    assert "Неверный логин или пароль" in error_message
+    driver.quit()
